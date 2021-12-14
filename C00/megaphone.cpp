2c48a432e2b847f4459cf617385766ddafaed298
Tanguy Billon <tanguy_billon@hotmail.fr>
2021-12-14 11:29:58 +0100

    megaphone ok

--- C00/megaphone.cpp
+++ C00/megaphone.cpp
@@ -6,17 +6,22 @@
 /*   By: Tanguy <Tanguy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 11:00:27 by Tanguy            #+#    #+#             */
-/*   Updated: 2021/12/14 11:03:20 by Tanguy           ###   ########.fr       */
+/*   Updated: 2021/12/14 11:29:27 by Tanguy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
+#include <cctype>
 
 int	main(int ac, char **av)
 {
 	if (ac > 1)
 	{
-
+		for (int i = 1; i < ac; i++)
+			for (int j = 0; j < (int)strlen(av[i]); j++)
+				std::cout << (char)toupper(av[i][j]);
+			std::cout << std::endl;
 	}
 	else
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
