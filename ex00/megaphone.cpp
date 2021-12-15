d1b3a7f496f10fed086c7d96c581038199f2daf6
Tanguy Billon <tanguy_billon@hotmail.fr>
2021-12-15 09:11:08 +0100

    file and folder

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Tanguy <Tanguy@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/14 11:00:27 by Tanguy            #+#    #+#             */
+/*   Updated: 2021/12/14 11:44:13 by Tanguy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <cctype>
+
+int	main(int ac, char **av)
+{
+	if (ac > 1)
+	{
+		for (int i = 1; i < ac; i++)
+			for (int j = 0; j < (int)strlen(av[i]); j++)
+				std::cout << (char)toupper(av[i][j]);
+			std::cout << std::endl;
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	return 0;
+}
