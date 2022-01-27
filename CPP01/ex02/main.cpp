b1ad63d50c743f2f8c8d6c9c2e5244c1c4b20ed2
Tanguy Billon <tanguy_billon@hotmail.fr>
2022-01-27 16:50:18 +0100

    update c++01

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tbillon <tbillon@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/25 14:07:28 by tbillon           #+#    #+#             */
-/*   Updated: 2022/01/25 14:13:55 by tbillon          ###   ########.fr       */
+/*   Updated: 2022/01/27 16:43:55 by tbillon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@ int main(void)
 	std::string*	stringPTR = &str;
 	std::string&	stringREF = str;
 
-	std::cout << "Memory address of str = " << &str << std::endl;
-	std::cout << "Memory address held by stringPTR = " << stringPTR << std::endl;
-	std::cout << "Memory address held by stringREF = " << &stringREF << std::endl;
-	std::cout << std::endl;
-	std::cout << "Memory address of str = " << str << std::endl;
-	std::cout << "Memory address held by stringPTR = " << *stringPTR << std::endl;
-	std::cout << "Memory address held by stringREF = " << stringREF << std::endl;
+	std::cout << "The memory address of the string variable = " << &str << std::endl;
+	std::cout << "The memory address held by stringPTR = " << stringPTR << std::endl;
+	std::cout << "The memory address held by stringREF = " << &stringREF << std::endl;
+	std::cout << "----------------------------------------------------------" << std::endl;
+	std::cout << "The value of the string variable = " << str << std::endl;
+	std::cout << "The value of the string stringPTR = " << *stringPTR << std::endl;
+	std::cout << "The value of the string stringREF = " << stringREF << std::endl;
 }
--- CPP01/ex06/Karen.hpp
+++ CPP01/ex06/Karen.hpp
@@ -6,7 +6,7 @@
 /*   By: tbillon <tbillon@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 09:51:07 by tbillon           #+#    #+#             */
-/*   Updated: 2022/01/26 10:05:03 by tbillon          ###   ########.fr       */
+/*   Updated: 2022/01/27 16:49:26 by tbillon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 #define INFO "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"
 #define WARNING "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month."
 #define ERROR "This is unacceptable! I want to speak to the manager now."
-#define ARGS_ERR "ERROR: provide only one level code following the executble -> ./karenFilter [LEVEL CODE]"
+#define ARGS_ERR "ERROR: Please provide one level code following the executble -> ./karenFilter [LEVEL CODE]"
 #define WRONG_CODE "ERROR: Please enter a valide level code -> DEBUG | INFO | WARNING | ERROR"
 
 class Karen
